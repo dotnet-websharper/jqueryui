5527a2ce464d84fdb747241948ba65caa59f53e9
atayanovskyy@e52ff631-ec73-4105-807b-774b3602857f <atayanovskyy@e52ff631-ec73-4105-807b-774b3602857f>
2010-04-21 14:55:15 +0000

    Published 1.0.15.
    --HG--
    extra : convert_revision : svn%3Ae52ff631-ec73-4105-807b-774b3602857f/trunk%4013

--- IntelliFactory.WebSharper.JQueryUI/AssemblyInfo.fs
+++ IntelliFactory.WebSharper.JQueryUI/AssemblyInfo.fs
@@ -1,6 +1,15 @@
 ﻿namespace IntelliFactory.WebSharper.JQueryUI
 
+open System.Reflection
 open IntelliFactory.WebSharper
+
+[<assembly: AssemblyCompany     "IntelliFactory">]
+[<assembly: AssemblyCopyright   "Copyright © IntelliFactory, 2004-2010">]
+[<assembly: AssemblyProduct     "IntelliFactory WebSharper™">]
+[<assembly: AssemblyTitle       "IntelliFactory.WebSharper.JQueryUI">]
+[<assembly: AssemblyName        "IntelliFactory.WebSharper.JQueryUI">]
+[<assembly: AssemblyDescription "JQuery UI WebSharper™ Extension">]
+[<assembly: AssemblyKeyFile     "//ifbuds01/pub/keys/IntelliFactory.snk">]
 
 [<assembly: WebSharper>]
 [<assembly: Require(typeof<Dependencies.JQueryBase>)>]
