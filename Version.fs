4effb4e19ffdf3ec28c0d1a27b0d0639348c2ed3
atayanovskyy@e52ff631-ec73-4105-807b-774b3602857f <atayanovskyy@e52ff631-ec73-4105-807b-774b3602857f>
2010-05-07 17:20:21 +0000

    Released 1.0.22.
    --HG--
    extra : convert_revision : svn%3Ae52ff631-ec73-4105-807b-774b3602857f/trunk%4020

--- Version.fs
+++ Version.fs
@@ -5,7 +5,7 @@ open System.Reflection
 #if DEBUG
 #else
 [<assembly: AssemblyVersion "1.0">]
-[<assembly: AssemblyInformationalVersion "1.0.21.0">]
-[<assembly: AssemblyFileVersion "1.0.21.*">]
+[<assembly: AssemblyInformationalVersion "1.0.22.0">]
+[<assembly: AssemblyFileVersion "1.0.22.*">]
 do ()
 #endif
