e98b9fbc13368c935b706dbe5c06d53a639f35ab
atayanovskyy@e52ff631-ec73-4105-807b-774b3602857f <atayanovskyy@e52ff631-ec73-4105-807b-774b3602857f>
2010-10-28 12:26:49 +0000

    Fixed CSS loading to use Google CDN by default.
    --HG--
    branch : 2.0
    extra : convert_revision : svn%3Ae52ff631-ec73-4105-807b-774b3602857f/branches/2.0%4033

--- IntelliFactory.WebSharper.JQueryUI/Dependencies.fs
+++ IntelliFactory.WebSharper.JQueryUI/Dependencies.fs
@@ -15,42 +15,35 @@ namespace IntelliFactory.WebSharper.JQueryUI
 module Resources =
     open IntelliFactory.WebSharper
     open System.Configuration
-            
-    let JQueryUIBase =            
+
+    let JQueryUIBase =
         match ConfigurationManager.AppSettings.["IntelliFactory.WebSharper.JQueryUI"] with
-        | null ->
-            "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1"
-        | url ->
-            url
+        | null  -> "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1"
+        | url   -> url
 
     let JQueryUIBaseCss =
         match ConfigurationManager.AppSettings.["IntelliFactory.WebSharper.JQueryUICss"] with
-        | null ->
-            "http://jquery-ui.googlecode.com/svn/tags/1.8rc3/themes/base"
-        | url ->
-            url     
-        
-        
-    type ModuleResource = {Js: string}
-        with
-            interface IResource with            
-                member this.Id = this.Js
-                member this.Dependencies = Seq.empty
-                member this.Render context writer =
-                    let loc =
-                        sprintf "%s/%s.js" JQueryUIBase this.Js
-                    Resources.RenderJavaScript loc writer
-        
-    type ModuleCssResource = {Css: string}
-        with
-            interface IResource with
-                member this.Id = this.Css
-                member this.Dependencies = Seq.empty
-                member this.Render context writer =
-                    let loc = sprintf "%s/%s.css" JQueryUIBaseCss this.Css
-                    Resources.RenderCss loc writer
+        | null  -> "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/themes/base"
+        | url   -> url
 
-    type JQueryUIAllJS() =          
+    type ModuleResource = {Js: string} with
+        interface IResource with
+            member this.Id = this.Js
+            member this.Dependencies = Seq.empty
+            member this.Render context writer =
+                let loc =
+                    sprintf "%s/%s.js" JQueryUIBase this.Js
+                Resources.RenderJavaScript loc writer
+        
+    type ModuleCssResource = {Css: string} with
+        interface IResource with
+            member this.Id = this.Css
+            member this.Dependencies = Seq.empty
+            member this.Render context writer =
+                let loc = sprintf "%s/%s.css" JQueryUIBaseCss this.Css
+                Resources.RenderCss loc writer
+
+    type JQueryUIAllJS() =
         inherit Attributes.RequireAttribute()
             override this.Resource ={Js = "jquery-ui"} :> IResource
 
