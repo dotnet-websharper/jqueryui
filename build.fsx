297b9dbdfec536c4ecfcecd7cbc1764f4894674a
András Jankó <andras.janko@gmail.com>
2015-03-31 19:39:39 +0200

    remove alpha tag

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.JQueryUI", "3.0-alpha")
+    BuildTool().PackageId("WebSharper.JQueryUI", "3.0")
         .References(fun r -> [r.Assembly "System.Web"])
     |> fun bt -> bt.WithFramework(bt.Framework.Net40)
 
