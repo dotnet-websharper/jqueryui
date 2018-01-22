ba7c0a74838ce9244c8865d5935a70f44030bd5d
Loic Denuziere <loic@denuziere.net>
2018-01-22 14:31:18 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,42 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.JQueryUI")
-        .VersionFrom("WebSharper")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
-        .References(fun r ->
-            [
-                r.Assembly "System.Web"
-                r.NuGet("WebSharper.Html").Latest(true).ForceFoundVersion().Reference()
-            ])
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    bt.WebSharper4.Library("WebSharper.JQueryUI")
-        .SourcesFromProject()
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-let test =
-    bt.WebSharper4.HtmlWebsite("WebSharper.JQueryUI.Tests")
-        .SourcesFromProject()
-        .References(fun r -> [
-                r.Project main
-                r.NuGet("WebSharper.Html").Latest(true).ForceFoundVersion().Reference()
-            ])
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-bt.Solution [
-    main
-    test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.JQueryUI-1.11.1"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://github.com/intellifactory/websharper.jqueryui"
-                Description = "WebSharper Extensions for JQueryUI 1.11.1"
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
